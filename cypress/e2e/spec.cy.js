fd003c2499100025fe2b6121098195a359673e90
Antti Ketola <antti@anttiketola.com>
2023-09-12 18:29:17 +0300

    Exercise 11.9 done

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,11 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('navigating to a single pokemon page works', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('clefairy').click()
+    cy.contains('clefairy')
+    cy.contains('cute charm')
   })
 })
